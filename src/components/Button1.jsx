61b74bed62999a4afdc61ef518da53e32bd3dccb
Carolina-Ceron <carol.ceron801@gmail.com>
2025-05-23 18:05:51 -0300

    Creación del componente Button1

---
+++ src/components/Button1.jsx
@@ -0,0 +1,15 @@
+import "../styles/Button1.css"; 
+
+function Button1({ title, type= "button",onClick, className = "" }) {
+    return (
+    <button 
+    type={type}
+    onClick={onClick}
+    className={`button1 ${className}`} 
+    >
+    {title}
+    </button>
+    );
+}
+
+export default Button1
