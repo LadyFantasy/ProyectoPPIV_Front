a1ccf810b258942788ecfc469e4b4bedd64c0994
LadyFantasy <msoledadm88@gmail.com>
2025-06-30 21:59:48 +0200

    tests selenium

--- tests/tests/test_loginexitoso.py
+++ tests/tests/test_loginexitoso.py
@@ -6,22 +6,24 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
-class TestLoginnoexitosopasserr():
+class TestLoginexitoso():
   def setup_method(self, method):
-    self.driver = webdriver.Firefox()
+    service = Service(ChromeDriverManager().install())
+    self.driver = webdriver.Chrome(service=service)
     self.vars = {}
   
   def teardown_method(self, method):
     self.driver.quit()
   
-  def test_loginnoexitosopasserr(self):
-    self.driver.get("https://proyecto-ppiv-front.vercel.app//admin-panel")
-    self.driver.set_window_size(1181, 797)
-    self.driver.find_element(By.CSS_SELECTOR, ".navbar__logout > svg").click()
+  def test_loginexitoso(self):
+    self.driver.get("https://tpi-devops-git-main-ladyfantasys-projects.vercel.app/login")
+    self.driver.set_window_size(1936, 1048)
     self.driver.find_element(By.CSS_SELECTOR, ".login-input-top").click()
     self.driver.find_element(By.CSS_SELECTOR, ".login-input-top").send_keys("germangp62@gmail.com")
     self.driver.find_element(By.CSS_SELECTOR, ".login-input-bottom").click()
-    self.driver.find_element(By.CSS_SELECTOR, ".login-input-bottom").send_keys("123456789")
+    self.driver.find_element(By.CSS_SELECTOR, ".login-input-bottom").send_keys("1234")
     self.driver.find_element(By.CSS_SELECTOR, ".button1").click()
   
